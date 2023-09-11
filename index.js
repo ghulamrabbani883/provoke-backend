c5b2baff088b8e75557e55297e7204bc4cc5ef6a
Ghulam Rabbani Ansari <ghulamrabbani883@gmail.com>
2023-09-11 21:41:23 +0530

    origin added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({credentials:true, origin: 'http://localhost:5173'}))
+app.use(cors({credentials:true, origin: 'https://provoke-front.vercel.app'}))
 
 
 app.get('/', (req,res)=>{
